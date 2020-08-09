e654e068262498e9398bad326cffb46b95bf4fea
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-08-09 00:16:01 -0300

    Rotas de conexões + controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/classesControler';
+import ConnectionsControlller from './controllers/connnectionsController';
 
 const routes = express.Router();
-const classControllers = new ClassesController()
+const classControllers = new ClassesController();
+const connectionsController = new ConnectionsControlller();  
 
 routes.get('/classes', classControllers.index);
 routes.post('/classes', classControllers.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
